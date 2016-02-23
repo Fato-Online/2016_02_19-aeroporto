81c17e4d3292a317e1bcf3d3aa0d224b78a73dfb
Fabricio Assis <fabricio.assis@fatoonline.com.br>
2016-02-22 21:57:20 -0300

    Correção menu, nome das sessões e js pessoal

---
+++ js/pessoal.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+//inicio
+	$("#inicio").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-inicio").offset().top}, "slow");
+	});
+
+//terminal
+	$("#terminal").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-terminal").offset().top}, "slow");
+	});
+
+//seguranca
+	$("#seguranca").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-seguranca").offset().top}, "slow");
+	});
+
+//torre
+	$("#torre").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-torre").offset().top}, "slow");
+	});
+
+
+//vip
+	$("#vip").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-vip").offset().top}, "slow");
+	});
+
+//esteiras
+	$("#esteiras").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-esteiras").offset().top}, "slow");
+	});
+
+//cco
+	$("#cco").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-cco").offset().top}, "slow");
+	});
+
+//dufry
+	$("#dufry").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-dufry").offset().top}, "slow");
+	});
+
+//receita
+	$("#receita").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-receita").offset().top}, "slow");
+	});
+
+//vigiario
+	$("#vigiario").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-vigiario").offset().top}, "slow");
+	});
+
+//federal
+	$("#federal").click(function() {
+		$('body,html').animate({scrollTop: $(".sec-federal").offset().top}, "slow");
+	});
+
+});
